7e3eb2f45791a50c7c95b3c07c6bdebce223e355
Gary Bisson <gary@osomprivacy.com>
2021-10-08 10:23:38 +0200

    input: fingerprint: fix for GKI build
    - all objects must be considered as one entity otherwise the license
    check will fail on platform.c/netlink.c.
    - wakeup_source_destroy() is not exposed in the abi, should use
    unregister() instead anyway
    - devm_gpio_free isn't needed that the whole purpose of devm_gpio_* is
    to free resources automatically
    
    Signed-off-by: Gary Bisson <gary@osomprivacy.com>
    Change-Id: Ie758f1eecebae9a9119b60592087e254e9e0f609

--- drivers/input/fingerprint/gf_spi.c
+++ drivers/input/fingerprint/gf_spi.c
@@ -808,7 +808,7 @@ static int gf_remove(struct platform_device *pdev)
 {
 	struct gf_dev *gf_dev = &gf;
 
-	wakeup_source_destroy(fp_wakelock);
+	wakeup_source_unregister(fp_wakelock);
 	fb_unregister_client(&gf_dev->notifier);
 	if (gf_dev->input)
 		input_unregister_device(gf_dev->input);
